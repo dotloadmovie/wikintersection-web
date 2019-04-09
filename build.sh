0e83efc4b55b6a4ff3cc61bb7470c1271484b569
David Tickle <david.tickle@thomsonreuters.com>
2019-04-09 14:53:23 +0100

    adding build step

--- build.sh
+++ build.sh
@@ -1,13 +1,17 @@
+echo "beginning UI build"
 cd ui/wikintersect
 npm install
 npm run build
 node deploy.js
 
+echo "merging built UI into static paths"
 mv build static
 cp -R static ../../
 cd ../../
 cp static/index.html public/views/index.html
 
+echo "build server"
 go build main.go
 
+echo "complete"
 exit 1
